d9c62820876436335ba24c8561a01efe56b4d8ac
Sebas <segonpin@gmail.com>
2017-10-31 19:02:19 +0100

    git_add_doc script

---
+++ git_add_doc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# this is used to stop the script if something fails
+set -e
+
+git checkout gh-pages
+cp -r docs/build/html/* .
+cp -r docs/doxygen/html doxygen/
+git add *.html
+git add searchindex.js
+git add -f _images
+git add -f _static
+git add -f _sources
+git add -f doxygen
+git add -f userguide
+git add -f devguide
+git add -f methods
+
+git commit -am 'update the documentation'
+git push origin gh-pages
+
+#git checkout master
+
+
